e6a3a4caee06f96e6788b85a3c70331c6f6d2f40
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2023-10-29 17:01:38 +0530

    agecalc

---
+++ Web/AgeCalc/age.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("#btn");
+ 
+
+function calculateAge() {
+    const birthDateInput = document.getElementById("date");
+    const resultElement = document.getElementById("result");
+  
+    const birthDate = new Date(birthDateInput.value);
+    const today = new Date();
+  
+    const ageInMilliseconds = today - birthDate;
+    const ageInYears = ageInMilliseconds / (365 * 24 * 60 * 60 * 1000);
+  
+    resultElement.textContent = `You age is ${ageInYears.toFixed(2)}.`;
+  }
+
+  btn.addEventListener("click", calculateAge);
